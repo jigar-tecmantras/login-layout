a3a4225a41c671d0110dc2ef3429bd8f9df1cd1b
jigar-tecmantras <73215263+jigar-tecmantras@users.noreply.github.com>
2026-07-16 13:12:01 +0530

    Initial frontend login page layout

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginPage from './LoginPage';
+
+function App() {
+  return (
+    <div className="app-shell">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
